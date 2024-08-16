e2e0d53e0878e267118ce1f4bec58ddc34207e53
avokeys <alexander.ayala.palacin@gmail.com>
2024-08-16 01:09:51 +0000

    end routes working for sign up and login with JWT

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,13 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+import hashlib
+from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
+
 
 api = Blueprint('api', __name__)
 
@@ -12,11 +19,49 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/login', methods=['POST'])
+def login():
+    email = request.json.get('email')
+    password = request.json.get('password')
+    expiration = datetime.timedelta(days=3)
+    user = User.query.filter_by(email=email).first()
+    if not user:
+        return jsonify("Email/Password are incorrect"),401
+    if not check_password_hash(user.password,password):
+        return jsonify("Email/Password are incorrect"),401
+    
+    access_token = create_access_token(identity=email,expires_delta=expiration)
+    return jsonify(access_token=access_token),200
+    
+@api.route("/signup", methods=["POST"])
+def signup():
+    name = request.json.get("name", None)
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(email=email).first()
+    if user:
+        return jsonify({"msg": "user already exist"}), 401
+    new_user = User(
+        name=name,
+        email=email,
+        password= generate_password_hash(password)
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return jsonify("user created successfully")
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+@api.route("/users", methods=["GET"])
+def get_users():
+    users_list = User.query.all() 
+    users_serialized = [user.serialize() for user in users_list] 
+    return jsonify(users_serialized), 200
 
-    return jsonify(response_body), 200
+
+
+@api.route("/user", methods=["GET"])
+@jwt_required()
+def get_user():
+    user_email = get_jwt_identity() 
+    user = User.query.filter_by(email=user_email).first()
+    return jsonify(user.serialize())
